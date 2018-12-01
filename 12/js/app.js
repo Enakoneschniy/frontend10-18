30961433adc59cd66107356cadaabc29798f0172
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2018-12-01 12:37:51 +0200

    lesson 12 part 02

--- 12/js/app.js
+++ 12/js/app.js
@@ -30,7 +30,35 @@
             console.log(`Hello! ${fullInfo.bind(this)()}`);
         }
     }
+    class User {
+       constructor({ firstName = 'Vasya', lastName = 'Pupkin' } = {}) {
+           this.firstName = firstName;
+           this.lastName = lastName;
+       }
+       get fullName() {
+           return `${this.firstName} ${this.lastName}`;
+       }
+       set fullName(value) {
+            [this.firstName, this.lastName] = value.trim().split(' ');
+       }
+    }
 
+    class Cat extends Animal {
+        constructor({ name = 'No name', weight = 0, gender = 'male', age = 0 } = {}) {
+            super({ name, weight, gender });
+            this.age = age;
+        }
+    }
+
+    const cat = new Cat({ name: 'Petya', weight: 15 });
+    console.log(cat);
+
+
+    const user = new User();
+    console.log(user.fullName);
+    user.fullName = `Ivan Ivanov`;
+    console.log(user.fullName);
+    console.log(user);
     const animal = new Animal();
     console.log(animal);
     animal.sayHello();
@@ -42,6 +70,55 @@
         console.log(this);
     }.bind(animal));
 
+    class Product {
+        constructor({
+            title = 'Default title',
+            description = 'Description',
+            price = 0,
+            quantity = 0,
+            image = ''
+        } = {}) {
+            this.title = title;
+            this.description = description;
+            this.image = image;
+            this.price = price;
+            this.quantity = quantity;
+        }
+    }
+
+    class Phone extends Product {
+        constructor({
+            title = 'Default title',
+            description = 'Description',
+            price = 0,
+            quantity = 0,
+            image = '',
+            color = 'black',
+            ram = 8,
+            storage = 128
+        } = {}) {
+            super({title, description, price, quantity, image});
+            this.storage = storage;
+            this.ram = ram;
+            this.color = color;
+        }
+    }
+
+    class Wine extends Product {
+        constructor({
+            title = 'Default title',
+            description = 'Description',
+            price = 0,
+            quantity = 0,
+            image = '',
+            volume = 1,
+            alc = 13
+        } = {}) {
+            super({title, description, price, quantity, image});
+            this.volume = volume;
+            this.alc = alc;
+        }
+    }
 })();
 
 
