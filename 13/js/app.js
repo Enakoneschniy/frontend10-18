c3f1c43c7bbed372c33341372dd979b4d9bc23b7
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2018-12-08 12:36:44 +0200

    lesson 13 part 02

--- 13/js/app.js
+++ 13/js/app.js
@@ -40,7 +40,7 @@
     }
     async function x2(a, b) {
         let sum = 0;
-        for (let i = 0; i < 100000000; i++) {
+        for (let i = 0; i < 1000000; i++) {
             sum *= Math.sqrt(sum) + i;
         }
         return a / b;
@@ -50,7 +50,7 @@
         const res = await x2(30, 3);
         console.log('Resolve 1: ', res);
     }
-    x3();
+    // x3();
     x1(20, 2).then((res) => {
         console.log('Resolve 1: ', res);
         return x1(res, 2);
@@ -69,5 +69,56 @@
     console.log('await 1: ', res);
     res = await x1(res, 2);
     console.log('await 2: ', res);
+
+})();
+
+(() => {
+    'use strict';
+
+    async function getProducts() {
+        const response = await (await fetch('https://next.json-generator.com/api/json/get/E1ztOIgxb')).json();
+        return response.map(item => {
+            item.price = parseFloat(item.price);
+            return item;
+        });
+    }
+    function renderProducts(products) {
+        const mainTag = document.getElementById('main');
+        const productList = document.createElement('ul');
+        products.forEach(product => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+               <h2>${product.title}</h2>
+               <span>Price: ${product.price} UAH</span>
+               <button class="buy-btn" data-product="${product.id}">Buy</button>
+            `;
+            productList.appendChild(li);
+        });
+        mainTag.appendChild(productList);
+    }
+    function querySelectorAll(selector) {
+        return [].slice.call(document.querySelectorAll(selector));
+    }
+    function setOnBuyClick() {
+        const btns = querySelectorAll('.buy-btn');
+        btns.forEach(btn => {
+            btn.addEventListener('click', onClick);
+        });
+
+    }
+    function onClick(event) {
+        const productId = event.target.getAttribute('data-product');
+        addToBasket(productId);
+        event.target.removeEventListener('click', onClick);
+    }
+    function addToBasket(id, quantity = 1) {
+        alert(id);
+    }
+    async function main() {
+        const products = await getProducts();
+        renderProducts(products);
+        setOnBuyClick();
+    }
+    main();
 
 })();
